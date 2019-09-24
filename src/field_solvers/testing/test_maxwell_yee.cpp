51274f667d4bef38ef039890a855cd01331315cf
Irene Garnelo <irga@irgapc.ipp.mpg.de>
2019-09-24 09:16:44 +0200

    maxwell 3D first order

--- src/field_solvers/testing/test_maxwell_yee.cpp
+++ src/field_solvers/testing/test_maxwell_yee.cpp
@@ -1,6 +1,9 @@
 //------------------------------------------------------------------------------
-// This example is a copy of the demo "HeatEquation_EX1_C" from AmreX
-// some changes have been made: rewriting fortran-functions into c++
+// Test 3D Maxwell Yee Solver (finite differences) on periodic grid
+//
+//  We use the solution
+//  E(x,t) =  \begin{pmatrix} \cos(x_1+x_2+x_3 - \sqrt{3} t) \\ -2\cos(x_1+x_2+x_3) \\ \cos(x_1+x_2+x_3 - \sqrt{3} t) \end{pmatrix}
+//  B(x,t) = \begin{pmatrix} \sqrt{3} \cos(x_1+x_2+x_3 - \sqrt{3} t) \\ 0 \\ -\sqrt{3} \cos(x_1+x_2+x_3 - \sqrt{3} t) \end{pmatrix}
 //------------------------------------------------------------------------------
 
 #include <AMReX.H>
@@ -13,18 +16,18 @@
 using namespace std;
 using namespace amrex;
 //------------------------------------------------------------------------------
-// init phi
-void init_phi(amrex::IntVect  lo,
+// init E and B
+void init_E_B(amrex::IntVect  lo,
 	      amrex::IntVect  hi,
-	      FArrayBox &phi,
-	      //amrex::MultiFab phi,
+	      FArrayBox &E_new,
+	      FArrayBox &E_sol,
+	      FArrayBox &B_new,
+	      FArrayBox &B_sol,
 	      const double  dx[3],
-	      const double  prob_lo[3],
-	      const double  prob_hi[3]) {
+	      const double  prob_lo[3]) {
   
   int k,j,i;
-  double x,y,z,r2;
-
+  double x,y,z;
   
   for(int k=lo[2]; k<=hi[2]; k++){
     z = prob_lo[2] + ((double)k+0.5) * dx[2];
@@ -32,165 +35,160 @@ void init_phi(amrex::IntVect  lo,
       y = prob_lo[1] + ((double)j+0.5) * dx[1];
       for(int i=lo[0]; i<=hi[0]; i++){
         x = prob_lo[0] + ((double)i+0.5) * dx[0];
-	r2 = pow((x-0.25),2.0) + pow((y-0.25),2.0) + pow((z-0.25),2.0) / 0.01;
-	// the box for this value of phi:
+	
+	// the box for these values:
 	Box cc({i,j,k}, {i+1,j+1,k+1});
-        phi.setVal(1.0 + exp(-r2), cc);
+	
+        E_new.setVal(cos(x+y+z), cc, 0, 1);
+	E_new.setVal(-2*cos(x+y+z), cc, 1, 1);
+	E_new.setVal(cos(x+y+z), cc, 2, 1);
+
+	B_new.setVal(sqrt(3)*cos(x+y+z), cc, 0, 1);
+	B_new.setVal(0, cc, 1, 1);
+	B_new.setVal(-sqrt(3)*cos(x+y+z), cc, 2, 1);
+	
       }
     }
   }
+
+  // at time 0 sol = new
+  // arguments: Fab to copy, sorce comp, copy comp, number of comps
+  B_sol.copy(B_new,0,0,3);
+  
+  E_sol.copy(E_new,0,0,3);
   
  }
 
 //------------------------------------------------------------------------------
-// update phi
-void update_phi(amrex::IntVect lo,
+// update E and B
+void update_E_B(amrex::IntVect lo,
 		amrex::IntVect hi,
-		FArrayBox &phiold,
-		FArrayBox &phinew,
-		FArrayBox &fluxx,
-		FArrayBox &fluxy,
-		FArrayBox &fluxz,
+		FArrayBox &E_old,
+	        FArrayBox &E_new,
+	        FArrayBox &E_sol,
+		FArrayBox &B_old,
+	        FArrayBox &B_new,
+	        FArrayBox &B_sol,
 		const double dx[3],
-		Real dt) {
+		Real dt,
+		const double  prob_lo[3],
+	        Real time) {
   int i,j,k;
-  Real dtdx[3];
-  Real addphi;
-  Real valx0[1];
-  Real valx1[1];
-  Real valy0[1];
-  Real valy1[1];
-  Real valz0[1];
-  Real valz1[1];
-
-  dtdx[0] = dt/ dx[0];
-  dtdx[1] = dt/ dx[1];
-  dtdx[2] = dt/ dx[2];
-
-  phinew.copy(phiold);
+  double x,y,z;
+  Real c1[1];
+  Real c2[1];
+  Real c3[1];
+  Real c4[1];
+  Real c5[1];
 
   for(int k=lo[2]; k<=hi[2]; k++){
+    z = prob_lo[2] + ((double)k+0.5) * dx[2];
     for(int j=lo[1]; j<=hi[1]; j++){
+      y = prob_lo[1] + ((double)j+0.5) * dx[1];
       for(int i=lo[0]; i<=hi[0]; i++){
+	x = prob_lo[0] + ((double)i+0.5) * dx[0];
 
-	fluxx.getVal(valx0, {  i,  j,  k});
-	fluxx.getVal(valx1, {i+1,  j,  k});
+	// the box for these values:
+	Box cc({i,j,k}, {i+1,j+1,k+1});
+
+	// E,B
 	
-	fluxy.getVal(valy0, {  i,  j,  k});
-	fluxy.getVal(valy1, {  1,j+1,  k});
+	// E_x
+	E_old.getVal(c1, {  i,  j,  k},0,1);
+	B_old.getVal(c2, {  i,j+1,  k},2,1);
+	B_old.getVal(c3, {  i,  j,  k},2,1);
+	B_old.getVal(c4, {  i,  j,k+1},1,1);
+	B_old.getVal(c5, {  i,  j,  k},1,1);
+        E_new.setVal(*c1+dt*((*c2-*c3)/dx[1]-(*c4-*c5)/dx[2]), cc, 0, 1);
+
+	// E_y
+	E_old.getVal(c1, {  i,  j,  k},1,1);
+	B_old.getVal(c2, {  i,  j,k+1},0,1);
+	B_old.getVal(c3, {  i,  j,  k},0,1);
+	B_old.getVal(c4, {i+1,  j,  k},2,1);
+	B_old.getVal(c5, {  i,  j,  k},2,1);
+        E_new.setVal(*c1+dt*((*c2-*c3)/dx[2]-(*c4-*c5)/dx[0]), cc, 1, 1);
+
+	// E_z
+	E_old.getVal(c1, {  i,  j,  k},2,1);
+	B_old.getVal(c2, {i+1,  j,  k},1,1);
+	B_old.getVal(c3, {  i,  j,  k},1,1);
+	B_old.getVal(c4, {i  ,j+1,  k},0,1);
+	B_old.getVal(c5, {  i,  j,  k},0,1);
+        E_new.setVal(*c1+dt*((*c2-*c3)/dx[0]-(*c4-*c5)/dx[1]), cc, 2, 1);
+
+	// B_x
+	B_old.getVal(c1, {  i,  j,  k},0,1);
+	E_old.getVal(c2, {  i,j+1,  k},2,1);
+	E_old.getVal(c3, {  i,  j,  k},2,1);
+	E_old.getVal(c4, {  i,  j,k+1},1,1);
+	E_old.getVal(c5, {  i,  j,  k},1,1);
+        B_new.setVal(*c1-dt*((*c2-*c3)/dx[1]-(*c4-*c5)/dx[2]), cc, 0, 1);
+
+	// B_y
+	B_old.getVal(c1, {  i,  j,  k},1,1);
+	E_old.getVal(c2, {  i,  j,k+1},0,1);
+	E_old.getVal(c3, {  i,  j,  k},0,1);
+	E_old.getVal(c4, {i+1,  j,  k},2,1);
+	E_old.getVal(c5, {  i,  j,  k},2,1);
+        B_new.setVal(*c1-dt*((*c2-*c3)/dx[2]-(*c4-*c5)/dx[0]), cc, 1, 1);
+
+	// B_z
+	B_old.getVal(c1, {  i,  j,  k},2,1);
+	E_old.getVal(c2, {i+1,  j,  k},1,1);
+	E_old.getVal(c3, {  i,  j,  k},1,1);
+	E_old.getVal(c4, {i  ,j+1,  k},0,1);
+	E_old.getVal(c5, {  i,  j,  k},0,1);
+        B_new.setVal(*c1-dt*((*c2-*c3)/dx[0]-(*c4-*c5)/dx[1]), cc, 2, 1);
+
+        // Analytic values
+	E_sol.setVal(cos(x+y+z-sqrt(3)*time), cc, 0, 1);
+	E_sol.setVal(-2*cos(x+y+z), cc, 1, 1);
+	E_sol.setVal(cos(x+y+z-sqrt(3)*time), cc, 2, 1);
 	
-	fluxz.getVal(valz0, {  i,  j,  k});
-	fluxz.getVal(valz1, {  1,  j,k+1});
-
-
-	addphi = dtdx[0] * (valx1 - valx0) +
-                 dtdx[1] * (valy1 - valy0) +
-	         dtdx[2] * (valz1 - valz0);
-
-	Box cc( {i,j,k}, {i+1,j+1,k+1});
-	phinew.plus(addphi, cc);
+	B_sol.setVal(sqrt(3)*cos(x+y+z-sqrt(3)*time), cc, 0, 1);
+	B_sol.setVal(0, cc, 1, 1);
+	B_sol.setVal(-sqrt(3)*cos(x+y+z-sqrt(3)*time), cc, 2, 1);
+	
       }
     }
   }
   
-}
-//------------------------------------------------------------------------------
-// compute_flux
-
-void compute_flux(amrex::IntVect lo,
-		  amrex::IntVect hi,
-		  FArrayBox &phi,
-		  FArrayBox &fluxx,
-		  FArrayBox &fluxy,
-		  FArrayBox &fluxz,
-		  const double dx[3]) {
-  int i,j,k;
-  Real valx0[1];
-  Real valx1[1];
-  Real valy0[1];
-  Real valy1[1];
-  Real valz0[1];
-  Real valz1[1];
-  
-  // x-flux
-  for(int k=lo[2]; k<hi[2]; k++){
-    for(int j=lo[1]; j<hi[1]; j++){
-      for(int i=lo[0]; i<hi[0]+1; i++){
-	phi.getVal(valx0, {i,j,k});
-	phi.getVal(valx1, {i-1,j,k});
-	
-	Box cc( {i,j,k}, {i+1,j+1,k+1});
-	fluxx.setVal((*valx0 - *valx1)/dx[0], cc);
-      }
-    }
-  }
-  
-  // y-flux
-  for(int k=lo[2]; k<hi[2]; k++){
-    for(int j=lo[1]; j<hi[1]+1; j++){
-      for(int i=lo[0]; i<hi[0]; i++){
-	phi.getVal(valy0, {i,j,k});
-	phi.getVal(valy1, {i,j-1,k});
-	
-	Box cc( {i,j,k}, {i+1,j+1,k+1});
-	fluxy.setVal((*valy0 - *valy1)/dx[1],cc);
-      }
-    }
-  }
-
-  // z-flux
-  for(int k=lo[2]; k<hi[2]+1; k++){
-    for(int j=lo[1]; j<hi[1]; j++){
-      for(int i=lo[0]; i<hi[0]; i++){
-	phi.getVal(valz0, {i,j,k});
-	phi.getVal(valz1, {i,j,k-1});
-	
-	Box cc( {i,j,k}, {i+1,j+1,k+1});
-	fluxz.setVal((*valz0 - *valz1)/dx[2],cc);
-      }
-    }
-  }
 }
 
 //------------------------------------------------------------------------------
 // advance
-void advance (MultiFab& phi_old,
-	      MultiFab& phi_new,
-	      Array<MultiFab,3>& flux,
+void advance (MultiFab& E_old,
+	      MultiFab& E_new,
+	      MultiFab& E_sol,
+	      MultiFab& B_old,
+	      MultiFab& B_new,
+	      MultiFab& B_sol,
 	      Real dt,
-	      const Geometry& geom)
+	      const Geometry& geom,
+	      Real time)
 {
   // Fill the ghost cells of each grid from the other grids
     // includes periodic domain boundaries
-    phi_old.FillBoundary(geom.periodicity());
+    E_old.FillBoundary(geom.periodicity());
+    B_old.FillBoundary(geom.periodicity());
 
-    int Ncomp = phi_old.nComp();
-    int ng_p = phi_old.nGrow();
-    int ng_f = flux[0].nGrow();
+    int Ncomp = E_old.nComp();
+    int ng_p = E_old.nGrow();
 
-    const Real* dx = geom.CellSize(); 
-
+    const Real* dx = geom.CellSize();
     const Box& domain_bx = geom.Domain();
 
-    // Compute fluxes one grid at a time
-    for (MFIter mfi(phi_old); mfi.isValid(); ++mfi ) {
-      const Box& bx = mfi.validbox();
-
-      amrex::IntVect lo = {bx.smallEnd()};
-      amrex::IntVect hi = {bx.bigEnd()};
-      compute_flux(lo, hi, phi_old[mfi], flux[0][mfi], flux[1][mfi],
-		   flux[2][mfi], geom.CellSize());
-    }
-
     // Advance the solution one grid at a time
-    for (MFIter mfi(phi_old); mfi.isValid(); ++mfi ) {
+    for (MFIter mfi(E_old); mfi.isValid(); ++mfi ) {
       const Box& bx = mfi.validbox();
 
       amrex::IntVect lo = {bx.smallEnd()};
       amrex::IntVect hi = {bx.bigEnd()};
 
-      update_phi(lo, hi, phi_old[mfi], phi_new[mfi], flux[0][mfi], flux[1][mfi],
-		 flux[2][mfi], geom.CellSize(), dt);
+      update_E_B(lo, hi, E_old[mfi], E_new[mfi], E_sol[mfi], B_old[mfi],
+		 B_new[mfi], B_sol[mfi], geom.CellSize(), dt, geom.ProbLo(),
+		 time);
     }
 }
 
@@ -236,29 +234,32 @@ void main_main ()
     geom.define(domain,&real_box,CoordSys::cartesian,is_periodic);
   }
   int Nghost = 1; // number of ghost cells for each array
-  int Ncomp = 1;  // number of components for each array
+  int Ncomp = 3;  // number of components for each array
   DistributionMapping dm(ba); // how Boxes are distrubuted among MPI processes
 
 //------------------------------------------------------------------------------
-  // Initializing phi
+  // Initializing E & B
   
-  // we allocate two phi multifabs; one will store the old state, the other the new.
-  MultiFab phi_old(ba, dm, Ncomp, Nghost);
-  MultiFab phi_new(ba, dm, Ncomp, Nghost);
+  // we allocate three E/B multifabs; one will store the old state, one the new state,
+  // and one the analytic solution.
+  MultiFab E_old(ba, dm, Ncomp, Nghost);
+  MultiFab E_new(ba, dm, Ncomp, Nghost);
+  MultiFab E_sol(ba, dm, Ncomp, Nghost);
+
+  MultiFab B_old(ba, dm, Ncomp, Nghost);
+  MultiFab B_new(ba, dm, Ncomp, Nghost);
+  MultiFab B_sol(ba, dm, Ncomp, Nghost);
 
   // MFIter = MultiFab Iterator
-    for ( MFIter mfi(phi_new); mfi.isValid(); ++mfi )
+  for ( MFIter mfi(E_new); mfi.isValid(); ++mfi )
     {
         const Box& bx = mfi.validbox();
 
         amrex::IntVect lo = {bx.smallEnd()};
 	amrex::IntVect hi = {bx.bigEnd()};
-	const double * dx[3] = {geom.CellSize()};
-	const double * prob_lo[3] = {geom.ProbLo()};
-	const double * prob_hi[3] = {geom.ProbHi()};
 
-        init_phi(lo, hi, phi_new[mfi],
-		 geom.CellSize(), geom.ProbLo(), geom.ProbHi());
+        init_E_B(lo, hi, E_new[mfi], E_sol[mfi], B_new[mfi], B_sol[mfi],
+		 geom.CellSize(), geom.ProbLo());
 
     }
 
@@ -270,30 +271,37 @@ void main_main ()
 
     // time to start simulation
     Real time = 0.0;
-
-//------------------------------------------------------------------------------
-
-    // build flux multifabs
-    Array<MultiFab, 3> flux;
-    for (int dir = 0; dir < 3; dir++) {
-      // flux(dir) has one component, zero ghost cells, and is nodal in direction dir
-        BoxArray edge_ba = ba;
-        edge_ba.surroundingNodes(dir);
-        flux[dir].define(edge_ba, dm, 1, 0);
-    }
     
 //------------------------------------------------------------------------------
     // time loop
+    Vector<double> Eerr;
+    Vector<double> Berr;
     
     for (int n = 1; n <= nsteps; ++n)
     {
-        // old_phi <- new_phi from previous iter
-        MultiFab::Copy(phi_old, phi_new, 0, 0, 1, 0);
-	// advance phi
-	advance(phi_old, phi_new, flux, dt, geom);
+        // old_data <- new_data from previous iter
+        MultiFab::Copy(E_old, E_new, 0, 0, 3, 0);
+	MultiFab::Copy(B_old, B_new, 0, 0, 3, 0);
+	// advance E & B
+	advance(E_old, E_new, E_sol, B_old, B_new, B_sol, dt, geom, time+dt);
 	// advance time
 	time = time + dt;
 
+	// ERROR
+	// Subtract new from sol (Fab, start comp, num of comp, nghost):
+	E_sol.minus(E_new, 0, 3, 0);
+	B_sol.minus(B_new, 0, 3, 0);
+	
+	// Return max absolute value (components, nghost, local):
+	Eerr = E_sol.norm0({0,1,2}, 0, false);
+	Berr = B_sol.norm0({0,1,2}, 0, false);
+	
+	cout << "step " << n << endl;
+	cout << "Ex error: " << Eerr[0] << " |Ey error: " << Eerr[1] <<
+	  " |Ez error: " << Eerr[2] << endl;
+	cout << "Bx error: " << Berr[0] << " |By error: " << Berr[1] <<
+	  " |Bz error: " << Berr[2] << endl;
+      
     }
   
 }
