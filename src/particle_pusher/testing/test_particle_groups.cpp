15cea44aeeee3a3ab2bd32d9ddb4629d47206ef1
Irene Garnelo <irga@irgapc.ipp.mpg.de>
2019-10-08 15:11:23 +0200

    added an example particle class in particle_groups and a function to interpolate fields at particle positions in gempic_Particle_mod_K. This can be tested with the test added in the testing folder.

--- src/particle_pusher/testing/test_particle_groups.cpp
+++ src/particle_pusher/testing/test_particle_groups.cpp
@@ -2,17 +2,36 @@
 #include <AMReX.H>
 #include <AMReX_Print.H>
 #include <particle_groups.H>
-#include <infrastructure.H>
+#include <maxwell_yee.H>
+#include <gempic_Particle_mod_K.H>
 
 #include <AMReX_PlotFileUtil.H>
 #include <AMReX_ParmParse.H>
 #include <AMReX_Print.H>
+#include <AMReX_Particles.H>
 
 using namespace std;
 using namespace amrex;
 
+double E_x(double x,double y,double z, double t){return(cos(x+y+z-sqrt(3.0)*t));}
+double E_y(double x,double y,double z, double t){return(-2*cos(x+y+z-sqrt(3.0)*t));}
+double E_z(double x,double y,double z, double t){return(cos(x+y+z-sqrt(3.0)*t));}
+
+double B_x(double x,double y,double z, double t){return(sqrt(3)*cos(x+y+z-sqrt(3.0)*t));}
+double B_y(double x,double y,double z, double t){return(0);}
+double B_z(double x,double y,double z, double t){return(-sqrt(3)*cos(x+y+z-sqrt(3.0)*t));}
+
 void main_main ()
 {
+
+  // make pointer-array for functions
+  double (*fields[6]) (double x,double y,double z, double t);
+  fields[0] = E_x;
+  fields[1] = E_y;
+  fields[2] = E_z;
+  fields[3] = B_x;
+  fields[4] = B_y;
+  fields[5] = B_z;
 
 //------------------------------------------------------------------------------
   // Initialize Infrastructure
@@ -32,10 +51,81 @@ void main_main ()
   double twopi = 2.0*3.14159265359;
   RealBox real_box({0.0, 0.0, 0.0},
 		   {twopi, twopi, twopi});
+  // build infrastructure
   infrastructure infra(n_cell, max_grid_size, is_periodic, real_box);
 
+//------------------------------------------------------------------------------
+  //Initialize Particle Groups
+
+  const int n_species = 1;
+  array<Real, n_species> charge = {1.0};
+  array<Real, n_species> mass = {1.0};
+
+  particle_groups part_gr(infra, n_species, charge, mass);
+
+//------------------------------------------------------------------------------
+  //Initialize Maxwell Yee
+  int n_steps = 5;
+  maxwell_yee mw_yee(n_steps, real_box, infra);
+  mw_yee.init_E_B(fields, infra);
+
+//------------------------------------------------------------------------------
+  //Test CIC
+
   std::ofstream ofs("test_particle_groups.output", std::ofstream::out);
-  Print(ofs) << "particles" << endl;
+  Print(ofs) << "field_interpolation" << endl;
+  
+  Real tol = 0.01;
+    GpuArray<amrex::Real,3> plo;
+    plo[0] = infra.geom.ProbLo()[0];
+    plo[1] = infra.geom.ProbLo()[1];
+    plo[2] = infra.geom.ProbLo()[2];
+    GpuArray<amrex::Real,3> dxi;
+    dxi[0] = 1.0/infra.dx[0];
+    dxi[1] = 1.0/infra.dx[1];
+    dxi[2] = 1.0/infra.dx[2];
+
+    // fill ghost cells
+    (*mw_yee.E_Array[0]).FillBoundary(infra.geom.periodicity());
+    (*mw_yee.E_Array[1]).FillBoundary(infra.geom.periodicity());
+    (*mw_yee.E_Array[2]).FillBoundary(infra.geom.periodicity());
+
+    (*mw_yee.B_Array[0]).FillBoundary(infra.geom.periodicity());
+    (*mw_yee.B_Array[1]).FillBoundary(infra.geom.periodicity());
+    (*mw_yee.B_Array[2]).FillBoundary(infra.geom.periodicity());
+
+  for (ParIter<4,0,0,0> pti(*part_gr.mypc[0], 0); pti.isValid(); ++pti) {
+
+    const Box& box = pti.validbox();
+    auto& particles = pti.GetArrayOfStructs();
+    const long np  = pti.numParticles();
+    int nc = 3; // number of components
+
+    // interpolate one field at one particle position
+    Real eres[nc], bres[nc];
+    double esol, bsol;
+    for (int pp = 0; pp<np; pp++) {
+      for (int cc = 0; cc < nc; cc++){
+	//E-field
+	Array4<Real> const& earr = (*mw_yee.E_Array[cc])[pti].array();
+	eres[cc] = gempic_interpolate_cic(particles[pp], 1, earr, plo, dxi, //1=#comps
+				       *mw_yee.E_Index[cc], infra.n_cell);
+        esol = (*fields[cc])(particles[pp].pos(0),particles[pp].pos(1),particles[pp].pos(2),0.0);
+	if (abs(eres[cc]-esol) > tol) {
+	  Print(ofs) << "check results at particle " << pp << ", E-component " << cc <<  endl;
+	}
+
+	//B-field
+	Array4<Real> const& barr = (*mw_yee.B_Array[cc])[pti].array();
+	bres[cc] = gempic_interpolate_cic(particles[pp], 1, barr, plo, dxi, //1=#comps
+				       *mw_yee.B_Index[cc], infra.n_cell);
+        bsol = (*fields[cc+3])(particles[pp].pos(0),particles[pp].pos(1),particles[pp].pos(2),0.0);
+	if (abs(bres[cc]-bsol) > tol) {
+	  Print(ofs) << "check results at particle " << pp << ", B-component " << cc <<  endl;
+	}
+      }
+    }
+  }
   ofs.close();
 }
 
