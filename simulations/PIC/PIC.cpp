e8e52dfc5e36a2dbeb216cb0f1c7903e6ab35db1
Irene Garnelo <irga@irgapc.ipp.mpg.de>
2019-11-18 09:50:15 +0100

    generated initializer class that stores all parameters and functions in its attributes

--- simulations/PIC/PIC.cpp
+++ simulations/PIC/PIC.cpp
@@ -6,7 +6,6 @@
 #include <particle_mesh_coupling.H>
 #include <sampler.H>
 #include <initializer.H>
-#include <initializer2.H>
 #include <time_loop.H>
 
 #include <AMReX_PlotFileUtil.H>
@@ -17,28 +16,41 @@
 using namespace std;
 using namespace amrex;
 
+double WF (double x,double y,double z,double v_x,double v_y,double v_z,int Np) {
+  double k = 0.5;
+  double alpha = 0.5;
+  return((1.0 + alpha*cos(k*x))/Np);
+};
+
 void main_main ()
 {
   //------------------------------------------------------------------------------
   //build objects:
+
+  //initializer
+  initializer init;
+  int is_periodic[3] = {1,1,1};
+  init.initialize_from_parameters(8,4,is_periodic,0.01,1,1,{1.0},{1.0},1000,0.5,
+				  {0.0},{1.0},{1.0},WF);
     
   // infrastructure
-  infrastructure infra(n_cell, max_grid_size, is_periodic, real_box);
+  infrastructure infra(init.n_cell, init.max_grid_size, init.is_periodic, init.real_box);
 
   // empty cell-centered FAB for MFIter
   MultiFab IteratorFab(infra.grid, infra.distriMap, 1, 0);
 
   // maxwell_yee
-  maxwell_yee mw_yee(n_steps, real_box, infra, dt);
+  maxwell_yee mw_yee(init.n_steps, init.real_box, infra, init.dt);
   
   // particles
-  particle_groups part_gr(infra, n_species, charge, mass);
+  particle_groups part_gr(infra, init.n_species, init.charge, init.mass);
 
   //------------------------------------------------------------------------------
   // initialize particles:
   int species = 0; // all particles are same species for now
-  init_particles_cellwise(infra, &part_gr, Np_cell, species, {0.0}, {1.0}, {1.0}, weight_fun,
-			  &IteratorFab);
+  init_particles_cellwise(infra, &part_gr, init.Np_cell, species, init.Vmean, init.Vdev,
+  			  init.Vweight, init.weight_fun, &IteratorFab);
+  
   std::ofstream ofss("PIC_particle.output", std::ofstream::out);
   for (amrex::ParIter<4,0,0,0> pti(*part_gr.mypc[0], 0); pti.isValid(); ++pti) {
 
