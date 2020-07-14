3173189176775a1d2317637b42cb87a3e1d97691
irene-garnelo <irene.garnelo@ipp.mpg.de>
2020-07-14 17:11:15 +0200

    started adding a new function to generate diagnostic output based on WarpX

--- src/particle_pusher/testing/test_particle_groups.cpp
+++ src/particle_pusher/testing/test_particle_groups.cpp
@@ -1,3 +1,5 @@
+#include <tinyexpr.h>
+
 #include <AMReX.H>
 #include <AMReX_ParmParse.H>
 #include <AMReX_Particles.H>
@@ -8,15 +10,19 @@
 #include <GEMPIC_initializer.H>
 #include <GEMPIC_maxwell_yee.H>
 #include <GEMPIC_particle_groups.H>
+#include <GEMPIC_particle_positions.H>
 #include <GEMPIC_particle_mesh_coupling.H>
+#include <GEMPIC_sampler.H>
 
 using namespace std;
 using namespace amrex;
 using namespace Gempic;
 
 using namespace Field_solvers;
+using namespace Diagnostics_Output;
 using namespace Init;
 using namespace Particles;
+using namespace Sampling;
 
 void main_main ()
 {
@@ -26,7 +32,8 @@ void main_main ()
     //initializer
     initializer init;
     amrex::IntVect is_periodic(AMREX_D_DECL(1,1,1));
-    amrex::IntVect n_cell(AMREX_D_DECL(8,8,8));
+    amrex::IntVect n_cell(AMREX_D_DECL(4,4,4));
+    int max_grid_size = 2;
 
     std::array<std::vector<amrex::Real>, GEMPIC_VDIM> VM{};
     std::array<std::vector<amrex::Real>, GEMPIC_VDIM> VD{};
@@ -46,73 +53,37 @@ void main_main ()
     VW[2].push_back(1.0);
 #endif
 
-    init.initialize_from_parameters(n_cell,4,is_periodic,1,0.01,0,{1.0},{1.0},1000,0.5,VM,VD,VW);
+    std::string WF = "1.0 + 0.0 * cos(kvar * x)";
+    double x, y, z;
+    double k = 1.25;
+    int err;
+    te_variable read_vars[] = {{"x", &x}, {"y", &y}, {"z", &z}, {"kvar", &k}};
+    int varcount = 4;
+    te_expr *WF_parse = te_compile(WF.c_str(), read_vars, varcount, &err);
+
+
+    init.initialize_from_parameters(n_cell,max_grid_size,is_periodic,1,0.01,0,{1.0},{1.0},1,0.5,VM,VD,VW);
 
     // infrastructure
     infrastructure infra(init);
 
     // maxwell_yee
-    //maxwell_yee mw_yee(init, infra, init.Nghost);
+    maxwell_yee mw_yee(init, infra, init.Nghost);
 
     // particles
     particle_groups part_gr(init, infra);
 
     //------------------------------------------------------------------------------
     // initialize particles:
-    std::array<amrex::Real,GEMPIC_VDIM> velocity = {0.,0.,0.};
-    amrex::Real weight = 1.;
-    int species = 0;
-    std::array<amrex::Real,GEMPIC_SPACEDIM> position;
+    int species = 0; // all particles are same species for now
+    init_particles_cellwise(infra, part_gr, init, species, WF_parse, &x, &y, &z);
+    (*(part_gr).mypc[0]).Redistribute();
 
-    std::ofstream ofs("test_particle_groups.output", std::ofstream::out);
-    amrex::Print(ofs) << endl;
-
-    for (amrex::MFIter mfi= (*(part_gr).mypc[species]).MakeMFIter(0); mfi.isValid(); ++mfi ){
-        const amrex::Box& bx = mfi.validbox();
-        amrex::IntVect lo = {bx.smallEnd()};
-        amrex::IntVect hi = {bx.bigEnd()};
-
-        using ParticleType = amrex::Particle<GEMPIC_VDIM+1, 0>; // Particle template
-        auto& particles = (*(part_gr).mypc[species]).GetParticles(0)[std::make_pair(mfi.index(), mfi.LocalTileIndex())];
-
-#if (GEMPIC_SPACEDIM > 2)
-        for(int k=lo[2]; k<=hi[2]; k++){
-            position[2] = infra.geom.ProbLo()[2] + ((double)k+0.5)*infra.dx[2];
-#endif
-#if (GEMPIC_SPACEDIM > 1)
-            for(int j=lo[1]; j<=hi[1]; j++){
-                position[1] = infra.geom.ProbLo()[1] + ((double)j+0.5)*infra.dx[1];
-#endif
-                for(int l=lo[0]; l<=hi[0]; l++){
-                    position[0] = infra.geom.ProbLo()[0] + ((double)l+0.5)*infra.dx[0];
-
-                    part_gr.add_particle(position, velocity, weight, particles);
-                }
-#if (GEMPIC_SPACEDIM > 1)
-            }
-#endif
-#if (GEMPIC_SPACEDIM > 2)
-        }
-#endif
-    }
-
-    //check positions
-    for (amrex::ParIter<GEMPIC_VDIM+1,0,0,0> pti(*part_gr.mypc[species], 0); pti.isValid(); ++pti) {
-
-        const auto& particles = pti.GetArrayOfStructs();
-        const long np = pti.numParticles();
-        for (int pp=0;pp<np;pp++) {
-            amrex::Print(ofs) << particles[pp].pos(0) << "," <<
-             #if (GEMPIC_SPACEDIM > 1)
-                         particles[pp].pos(1) << "," <<
-             #endif
-             #if (GEMPIC_SPACEDIM > 2)
-                         particles[pp].pos(2) <<
-             #endif
-                         std::endl;
-        }
-    }
-    ofs.close();
+    save_particle_positions(&part_gr, "ctest");
+    save_particle_velocities(&part_gr, "ctest");
+    std::cout << "A" << std::endl;
+   // WritePlotFile (&part_gr, &mw_yee, &infra, "test_wx_reader", 0);
+    std::cout << "B" << std::endl;
 
 }
 
