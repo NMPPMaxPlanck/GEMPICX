4dc81fd0cfb85d0748f9d9ffd400f7688ae5c749
irene-garnelo <irene.garnelo@ipp.mpg.de>
2021-01-19 17:43:47 +0100

    [ctest: one part]: removed redundant variables

--- src/particle_pusher/testing/test_one_part.cpp
+++ src/particle_pusher/testing/test_one_part.cpp
@@ -34,11 +34,6 @@ void main_main (bool ctest)
     // ------------------------------------------------------------------------------
     // ------------PARAMETERS--------------------------------------------------------
 
-    // compile parameters
-    std::array<int, GEMPIC_SPACEDIM> degs = {AMREX_D_DECL(degx, degy, degz)};
-    int maxdeg = *(std::max_element(degs.begin(), degs.end()));
-    int Nghost = maxdeg;
-
     // initialize parameters
     std::string sim_name = "One_Particle";
     std::array<int,GEMPIC_SPACEDIM> n_cell = {AMREX_D_DECL(4,4,4)};
@@ -65,7 +60,7 @@ void main_main (bool ctest)
     amrex::Real tolerance_particles = 1.e-10;
 
     std::array<std::vector<amrex::Real>, vdim> VM{};
-    std::array<std::vector<amrex::Real>, vdim> VD = {};
+    std::array<std::vector<amrex::Real>, vdim> VD{};
     std::array<std::vector<amrex::Real>, vdim> VW{};
 
     for (int j=0; j<vdim; j++) {
@@ -75,23 +70,6 @@ void main_main (bool ctest)
     VD[0].push_back(0.02/sqrt(2));
     VD[1].push_back(sqrt(12)*VD[0][0]);
     VD[2].push_back(VD[1][0]);
-
-
-    // functions
-    double x, y, z;
-    int err;
-    te_variable read_vars[] = {{"x", &x}, {"y", &y}, {"z", &z}, {"kvar", &k}};
-    int varcount = 4;
-    te_expr *WF_parse = te_compile(WF.c_str(), read_vars, varcount, &err);
-
-    te_expr *Bx_parse = te_compile(Bx.c_str(), read_vars, varcount, &err);
-    te_expr *By_parse = te_compile(By.c_str(), read_vars, varcount, &err);
-    te_expr *Bz_parse = te_compile(Bz.c_str(), read_vars, varcount, &err);
-
-    te_variable read_vars_poi[] = {{"x", &x}, {"y", &y}, {"z", &z}};
-    varcount = 3;
-    te_expr *rho_parse = te_compile(rho.c_str(), read_vars_poi, varcount, &err);
-    te_expr *phi_parse = te_compile(phi.c_str(), read_vars_poi, varcount, &err);
 
     // ------------------------------------------------------------------------------
     // ------------INITIALIZE GEMPIC-STRUCTURES--------------------------------------
@@ -106,15 +84,13 @@ void main_main (bool ctest)
     VlMa.VD = VD;
     VlMa.VW = VW;
 
-    VlMa.Nghost = 1;
-
     // infrastructure
     infrastructure infra;
     VlMa.initialize_infrastructure(&infra);
 
     // maxwell_yee
     maxwell_yee<vdim> mw_yee(VlMa, infra);
-    mw_yee.init_rho_phi(infra, phi_parse, rho_parse, &x, &y, &z);
+    mw_yee.init_rho_phi(infra, VlMa);
 
     // particles
     particle_groups<vdim, numspec> part_gr(VlMa, infra);
