bf196c48c19a77c1e853a6d2254b7fc9bf23104c
Eric Sonnendruecker <eric.sonnendruecker@ipp.mpg.de>
2021-05-20 11:49:00 +0200

    update cmake compilation on Cobra

--- scripts/compile_mpcdf.sh
+++ scripts/compile_mpcdf.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/sh
 SOURCE_DIRECTORY=`pwd`/../
 SOURCE_DIRECTORY=`readlink -f $SOURCE_DIRECTORY`
 AMREX_DIRECTORY=`pwd`/../../amrex
@@ -6,6 +7,7 @@ AMREX_DIRECTORY=`readlink -f $AMREX_DIRECTORY`
 echo $SOURCE_DIRECTORY
 echo $AMREX_DIRECTORY
 
+source /etc/profile.d/modules.sh
 module purge
 module load intel/19.1.1
 module load impi/2019.7
@@ -21,12 +23,12 @@ cd $BUILD_DIR
 mkdir -p amrex
 cd amrex
 
-cmake -D AMReX_SPACEDIM=3 -D AMReX_PARTICLES=ON -D CMAKE_BUILD_TYPE=Release -D CMAKE_C_COMPILER=mpiicc -D CMAKE_CXX_COMPILER=mpiicpc $AMREX_DIRECTORY
+cmake -D AMReX_SPACEDIM=3 -D AMReX_PARTICLES=ON -D CMAKE_BUILD_TYPE=Release -D CMAKE_C_COMPILER=icc -D CMAKE_CXX_COMPILER=icpc $AMREX_DIRECTORY
 
 make -j 10 install
 
 cd ..
 mkdir -p gempic
 cd gempic
-cmake -D AMReX_ROOT=$AMREX_DIRECTORY/installdir -D CMAKE_C_COMPILER=mpiicc -D CMAKE_CXX_COMPILER=mpiicpc  -D CMAKE_BUILD_TYPE=Release $SOURCE_DIRECTORY
+cmake  -D AMReX_ROOT=$AMREX_DIRECTORY/installdir -D CMAKE_C_COMPILER=mpiicc -D CMAKE_CXX_COMPILER=mpiicpc  -D CMAKE_BUILD_TYPE=Release $SOURCE_DIRECTORY
 make -j 10
