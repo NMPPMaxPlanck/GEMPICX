9477eef6973bb85eedf8822fd71e6ccaef97c988
Irene Garnelo Abellanas <irene.garnelo@ipp.mpg.de>
2021-09-07 09:44:52 +0200

    [AMReX ctest]: added atomic add test

---
+++ src/field_solvers/testing/test_AMReX_atomic_add.cpp
@@ -0,0 +1,94 @@
+/*------------------------------------------------------------------------------
+ Test 3D Maxwell Yee Solver (finite differences) on periodic grid
+
+  We test the solution
+  E(x,t) =  \begin{pmatrix} \cos(x_1+x_2+x_3 - \sqrt{3} t) \\
+                          -2\cos(x_1+x_2+x_3 - \sqrt(3) t) \\
+                            \cos(x_1+x_2+x_3 - \sqrt{3} t) \end{pmatrix}
+  B(x,t) = \begin{pmatrix} \sqrt{3} \cos(x_1+x_2+x_3 - \sqrt{3} t) \\
+                            0 \\
+                            -\sqrt{3} \cos(x_1+x_2+x_3 - \sqrt{3} t) \end{pmatrix}
+------------------------------------------------------------------------------*/
+
+#include <tinyexpr.h>
+
+#include <AMReX.H>
+#include <AMReX_ParmParse.H>
+#include <AMReX_PlotFileUtil.H>
+#include <AMReX_Print.H>
+
+#include <GEMPIC_Config.H>
+#include <GEMPIC_maxwell_yee.H>
+#include <GEMPIC_gempic_norm.H>
+#include <GEMPIC_vlasov_maxwell.H>
+#include <GEMPIC_sampler.H>
+#include <GEMPIC_assertion.H>
+
+using namespace std;
+using namespace amrex;
+using namespace Gempic;
+using namespace Field_solvers;
+using namespace Sampling;
+
+template<int vdim, int numspec, int degx, int degy, int degz>
+void main_main ()
+{
+    std::array<int,GEMPIC_SPACEDIM> n_cell = {AMREX_D_DECL(8,10,12)};
+    vlasov_maxwell<vdim, numspec> VlMa;
+    VlMa.set_params("add_test", n_cell, {1}, 10, 12, 12, 12, {AMREX_D_DECL(1, 1, 1)}, {AMREX_D_DECL(8, 10, 12)});
+    VlMa.set_computed_params();
+    CompDom::computational_domain infra;
+    VlMa.initialize_infrastructure(&infra);
+    particle_groups<vdim, numspec> part_gr(VlMa, infra);
+    const std::array<amrex::Real, 3>& dx = {0.6283, 0.5026, 0.4188};
+    const std::array<amrex::Real, 3>& plo = {0.0, 0.0, 0.0};
+    std::array<amrex::Real, 3> x = {0.2, 0.2, 0.2};
+    init_one_particle_cellwise<vdim>(dx, plo, &(*(part_gr).mypc[0]), x);
+
+    //------------------------------------------------------------------------------
+    amrex::MultiFab rho; // for Poisson
+    const amrex::BoxArray& nba = amrex::convert(infra.grid,amrex::IntVect::TheNodeVector());
+    amrex::Real Nghost = 1;
+    rho.define(nba, infra.distriMap, 1, Nghost);
+    rho.setVal(0.0);
+
+    amrex::Real testval = 2.3;
+    for (amrex::ParIter<vdim+1,0,0,0> pti(*(part_gr).mypc[0], 0); pti.isValid(); ++pti) {
+
+        amrex::Array4<amrex::Real> rhoarr = rho[pti].array();
+
+        amrex::Gpu::Atomic::Add(&(rhoarr)(5, 6, 7, 0), testval);
+
+    }
+    rho.SumBoundary(infra.geom.periodicity());
+
+    for ( amrex::MFIter mfi(rho); mfi.isValid(); ++mfi ) {
+        amrex::AllPrintToFile("test_AMReX_atomic_add_additional.tmp") << rho[mfi] << std::endl;
+    }
+
+
+    bool passed = true;
+    //gempic_assert(&passed, testval, rho(5,6,7,0));
+    amrex::AllPrintToFile("test_AMReX_atomic_add.tmp") << passed << std::endl;
+
+}
+
+int main(int argc, char* argv[])
+{
+    amrex::Initialize(argc,argv);
+
+#if (GEMPIC_SPACEDIM == 1)
+    main_main<1, 1, 1, 1, 1>();
+    main_main<2, 1, 1, 1, 1>();
+#elif (GEMPIC_SPACEDIM == 2)
+    main_main<2, 1, 1, 1, 1>();
+    main_main<3, 1, 1, 1, 1>();
+#elif (GEMPIC_SPACEDIM == 3)
+    main_main<3, 1, 1, 1, 1>();
+#endif
+    if (ParallelDescriptor::MyProc()==0) std::rename("test_ampere_faraday.tmp.0", "test_ampere_faraday.output");
+    amrex::Finalize();
+}
+
+
+
