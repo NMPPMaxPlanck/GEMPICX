d32ef2af4a5950e8646c50453f7b2f1897350a4c
irene-garnelo <irene.garnelo@ipp.mpg.de>
2021-10-25 15:20:19 +0200

    [compile errors]: fixed c++17 standard for gpu_raven2

--- scripts/compile_gpu_raven2.sh
+++ scripts/compile_gpu_raven2.sh
@@ -32,7 +32,7 @@ mkdir -p amrex
 mkdir -p amrex_install
 cd amrex
 
-cmake -D CMAKE_BUILD_TYPE=Release -D AMReX_GPU_BACKEND=CUDA -D AMReX_CUDA_ARCH=8.0 -D AMReX_SPACEDIM=3 -D AMReX_PARTICLES=ON -D CMAKE_CXX_COMPILER=mpiicpc -D CMAKE_C_COMPILER=mpiicc -D CUDA_HOST_COMPILER=mpiicpc -D CMAKE_CPP_COMPILER=mpiicpc -D CMAKE_LINKER=mpiicpc -D CMAKE_CUDA_FLAGS=-ccbin=mpiicpc -D CMAKE_INSTALL_PREFIX=../amrex_install --expt-extended-lambda $AMREX_DIRECTORY
+cmake -D CMAKE_BUILD_TYPE=Release -D CMAKE_CXX_FLAGS="-std=c++17" -D AMReX_GPU_BACKEND=CUDA -D AMReX_CUDA_ARCH=8.0 -D AMReX_SPACEDIM=3 -D AMReX_PARTICLES=ON -D CMAKE_CXX_COMPILER=mpiicpc -D CMAKE_C_COMPILER=mpiicc -D CUDA_HOST_COMPILER=mpiicpc -D CMAKE_CPP_COMPILER=mpiicpc -D CMAKE_LINKER=mpiicpc -D CMAKE_CUDA_FLAGS=-ccbin=mpiicpc -D CMAKE_INSTALL_PREFIX=../amrex_install --expt-extended-lambda $AMREX_DIRECTORY
 
 make install -j 16
 
@@ -41,6 +41,6 @@ rm -rf gempic
 mkdir -p gempic
 cd gempic
 
-CC=mpiicpc cmake -D AMReX_DIR=$HOME/gempic_gpu_obj3/amrex_install -D CMAKE_CXX_COMPILER=mpiicpc -D CMAKE_C_COMPILER=mpiicc -D USE_CUDA=ON -D CUDA_HOST_COMPILER=mpiicpc -D CMAKE_CPP_COMPILER=mpiicpc -D CMAKE_CUDA_FLAGS=-ccbin=mpiicpc -D CMAKE_MPI_CUDA_INCLUDE_DIRS='/mpcdf/soft/SLE_15/packages/x86_64/intel_parallel_studio/2020.2/compilers_and_libraries_2020.2.254/linux/mpi/intel64/include/'  -D CMAKE_CUDA_ARCHITECTURES=80 -D CMAKE_BUILD_TYPE=Release ~/gempic -D CMAKE_EXE_LINKER_FLAGS=-lnvToolsExt
+CC=mpiicpc cmake -D AMReX_DIR=$HOME/gempic_gpu_obj3/amrex_install -D CMAKE_CXX_FLAGS="-std=c++17" -D CMAKE_CXX_COMPILER=mpiicpc -D CMAKE_C_COMPILER=mpiicc -D USE_CUDA=ON -D CUDA_HOST_COMPILER=mpiicpc -D CMAKE_CPP_COMPILER=mpiicpc -D CMAKE_CUDA_FLAGS=-ccbin=mpiicpc -D CMAKE_MPI_CUDA_INCLUDE_DIRS='/mpcdf/soft/SLE_15/packages/x86_64/intel_parallel_studio/2020.2/compilers_and_libraries_2020.2.254/linux/mpi/intel64/include/'  -D CMAKE_CUDA_ARCHITECTURES=80 -D CMAKE_BUILD_TYPE=Release ~/gempic -D CMAKE_EXE_LINKER_FLAGS=-lnvToolsExt
 
 gmake -j 16
