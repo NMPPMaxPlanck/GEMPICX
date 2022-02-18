114968ed5a5f0dd263e71209cd1f855edce32581
Katharina Kormann <katharina.kormann@ipp.mpg.de>
2022-02-18 14:23:41 +0000

    Compiling

--- scripts/compile_desktop.sh
+++ scripts/compile_desktop.sh
@@ -1,25 +1,20 @@
+# Compile script for ubuntu desktop
+
+
 SOURCE_DIRECTORY=`pwd`/../
 SOURCE_DIRECTORY=`readlink -f $SOURCE_DIRECTORY`
-AMREX_DIRECTORY=`pwd`/../../amrex
-AMREX_DIRECTORY=`readlink -f $AMREX_DIRECTORY`
 
 echo $SOURCE_DIRECTORY
-echo $AMREX_DIRECTORY
+
+git submodule init
+git submodule update
 
 BUILD_DIR=$HOME/gempic_obj
 
 mkdir -p $BUILD_DIR
 cd $BUILD_DIR
 
-mkdir -p amrex
-cd amrex
-
-cmake -D AMReX_SPACEDIM=3 -D AMReX_PARTICLES=ON -D CMAKE_BUILD_TYPE=Release $AMREX_DIRECTORY
-
-make install -j 4
-
-cd ..
 mkdir -p gempic
 cd gempic
-cmake -D AMReX_DIR=$AMREX_DIRECTORY/installdir -D CMAKE_C_COMPILER=mpicc -D CMAKE_CXX_COMPILER=mpicxx -D CMAKE_BUILD_TYPE=Release $SOURCE_DIRECTORY
-make -j 4
+cmake -D CMAKE_BUILD_TYPE=Release $SOURCE_DIRECTORY
+make -j 2
