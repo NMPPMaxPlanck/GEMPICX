46c66b4188c407a5264bb02f6ebe38ebec6d9b6a
Emil Blaabjerg Poulsen <emilb.poulsen@ipp.mpg.de>
2025-06-02 13:10:17 +0000

    Workflow updates

--- scripts/CI_helpers.sh
+++ scripts/CI_helpers.sh
@@ -1,4 +1,4 @@
-# function for starting a collapsible section
+# Function for starting a collapsible section
 start_details () {
   details_id="${1}"
   details_title="${2:-$details_id}"
@@ -11,4 +11,39 @@ end_details () {
   details_id="${1}"
 
   echo "section_end:`date +%s`:${details_id}"$'\r\e[0K'
+}
+
+# Function for configure/build/test jobs
+configure_build_test () {
+  preset="${1}"
+  build_dir="${2:-build}"
+  numprocs="${3:-$(nproc)}"
+
+  start_details configure "CMake Configure Step"
+  cmake -S . -B $build_dir --preset $preset
+  end_details configure
+  start_details build "CMake Build Step"
+  cmake --build $build_dir -j $numprocs
+  end_details build
+  start_details test "Test Step"
+  cd $build_dir
+  ctest --output-on-failure --no-tests=error -j $numprocs
+  end_details test
+}
+
+configure_build_test_gpu () {
+  preset="${1}"
+  build_dir="${2:-build}"
+  numprocs="${3:-$(nproc)}"
+
+  start_details configure "CMake Configure Step"
+  cmake -S . -B $build_dir --preset $preset
+  end_details configure
+  start_details build "CMake Build Step"
+  cmake --build $build_dir -j $numprocs
+  end_details build
+  start_details test "Test Step"
+  cd $build_dir
+  ctest --output-on-failure --no-tests=error
+  end_details test
 }
