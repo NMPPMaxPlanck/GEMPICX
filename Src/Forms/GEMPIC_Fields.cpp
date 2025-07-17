708bfb20cc479dfb04f4d57815c19726d6ff1c89
Nils Schild <nils.schild@ipp.mpg.de>
2025-07-17 08:56:03 +0200

    Include ghost cells to multiplications with ghost cells.

--- Src/Forms/GEMPIC_Fields.cpp
+++ Src/Forms/GEMPIC_Fields.cpp
@@ -179,7 +179,7 @@ void operator*=(DiscreteVectorField& field, amrex::Real const& scalar)
         for (amrex::MFIter mfi{field.multi_fab(dir)}; mfi.isValid(); ++mfi)
         {
             field.select_box(mfi);
-            amrex::ParallelFor(mfi.validbox(), field.multi_fab(dir).nComp(),
+            amrex::ParallelFor(mfi.growntilebox(), field.multi_fab(dir).nComp(),
                                [=] AMREX_GPU_HOST_DEVICE(int ix, int iy, int iz, int n)
                                { field(dir, ix, iy, iz, n) *= scalar; });
         }
@@ -194,7 +194,7 @@ void operator+=(DiscreteVectorField& a, DiscreteVectorField const& b)
         {
             a.select_box(mfi);
             auto const& otherView{b.multi_fab(dir).array(mfi)};
-            amrex::ParallelFor(mfi.validbox(), a.multi_fab(dir).nComp(),
+            amrex::ParallelFor(mfi.growntilebox(), a.multi_fab(dir).nComp(),
                                [=] AMREX_GPU_HOST_DEVICE(int ix, int iy, int iz, int n)
                                { a(dir, ix, iy, iz, n) += otherView(ix, iy, iz, n); });
         }
