ba3781b03a8f36196e00d51b75f7a54ca6ac097a
Nils Schild <nils.schild@ipp.mpg.de>
2026-05-18 14:31:04 +0000

    Replace global compiler configurations by target based compiler configurations

--- cmake/gempic_CompilerConfig.cmake
+++ cmake/gempic_CompilerConfig.cmake
@@ -1,81 +1,56 @@
-message(STATUS "GEMPIC-CompilerID: ${CMAKE_CXX_COMPILER_ID}")
-if(CMAKE_CXX_COMPILER_ID MATCHES "GNU.*")
-  # gcc
-  set(CMAKE_CXX_FLAGS_RELEASE "${CMAKE_CXX_FLAGS_RELEASE} -funroll-all-loops -march=native")
-  set(CMAKE_CXX_FLAGS_DEBUG   "${CMAKE_CXX_FLAGS_DEBUG} -Wall")
-  message(STATUS "GEMPIC-Compiler: GNU")
-elseif(CMAKE_CXX_COMPILER_ID MATCHES "Intel.*")
-  # Intel compilers, new LLVM based versions (icx, icpx) as well as the legacy ones (icc, icpc) work well with these flags.
-  # Note that AVX512 is available on virtually all recent HPC-class x86_64 processors but not on Laptops and Desktops.
-  # Native optimization with the Intel compiler on AMD CPUs causes issues, so we set skylake-avx512 explicitly here:
-  set(CMAKE_CXX_FLAGS_RELEASE "${CMAKE_CXX_FLAGS_RELEASE} -march=skylake-avx512")
-  set(CMAKE_CXX_FLAGS_DEBUG   "${CMAKE_CXX_FLAGS_DEBUG} -Wall")
-  message(STATUS "GEMPIC-Compiler: Intel")
-  if(   ($ENV{HOSTNAME} MATCHES "raven*")
-     OR ($ENV{HOSTNAME} MATCHES "viper*")
-     OR (DEFINED ENV{MPCDF_RUNNER}))
-    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} --gcc-toolchain=$ENV{GCC_HOME}")
-    set(CMAKE_INSTALL_RPATH_USE_LINK_PATH ON)
+function(gempic_set_compile_options TARGET)
+  message(STATUS "GEMPIC-CompilerID: ${CMAKE_CXX_COMPILER_ID}")
+  string(TOLOWER ${CMAKE_BUILD_TYPE} BUILD_TYPE)
+  if(BUILD_TYPE MATCHES "release")
+    if(${CMAKE_CXX_COMPILER_ID} MATCHES "GNU.*")
+      target_compile_options(${TARGET} PRIVATE -funroll-all-loops -march=native)
+    endif()
   endif()
-elseif(CMAKE_CXX_COMPILER_ID MATCHES "Clang.*")
-  # Clang 
-  set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} -Wall -Wextra")
+  if(BUILD_TYPE MATCHES "debug")
+    target_compile_options(${TARGET} PRIVATE -Wall -Wextra)
+  endif()
   if(AMReX_SPACEDIM EQUAL 1)
-    set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} -Wno-braced-scalar-init")
-    set(CMAKE_CXX_FLAGS_RELEASE "${CMAKE_CXX_FLAGS_RELEASE} -Wno-braced-scalar-init")
+    target_compile_options(${TARGET} PRIVATE -Wno-braced-scalar-init)
   endif()
-  # MPCDF environments only provide full c++ std support for GCC.
-  # Point the clang implementation to the correct toolchain
-  if(   ($ENV{HOSTNAME} MATCHES "raven*")
-     OR ($ENV{HOSTNAME} MATCHES "viper*")
-     OR (DEFINED ENV{MPCDF_RUNNER}))
-    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} --gcc-toolchain=$ENV{GCC_HOME}")
-    set(CMAKE_INSTALL_RPATH_USE_LINK_PATH ON)
-  endif()
-  message(STATUS "GEMPIC-Compiler: Clang")
-else()
-  message(STATUS "GEMPIC-Compiler: Unknown compiler")
-endif()
 
-
-# NOTE: Enabling the link-time-optimization prolongs link times significantly!
-if(GEMPIC_USE_LTO)
-  include(CheckIPOSupported)
-  check_ipo_supported(RESULT IPO_IS_SUPPORTED OUTPUT IPO_ERROR_MSG)
-  if(IPO_IS_SUPPORTED)
-    message(STATUS "IPO/LTO enabled")
-    set(CMAKE_INTERPROCEDURAL_OPTIMIZATION TRUE) # All targets instantiated afterwards
-  else()
-    message(STATUS "IPO/LTO not supported: ${IPO_ERROR_MSG}")
-  endif()
-endif()
-
-if(GEMPIC_USE_HIP)
-  add_definitions(-DGEMPIC_GPU)
-endif()
-if(GEMPIC_USE_CUDA)
-  add_definitions(-DGEMPIC_GPU)
-  # Do we really only compile for GPU on raven?
-  # Flags should probably revisited
-  if($ENV{HOST} MATCHES "raven*")
-    # Are these flags reached at all? We do no compile GPU code with intel compiler
-    if(CMAKE_CXX_COMPILER_ID MATCHES "Intel.*")  
-      set(CMAKE_CUDA_FLAGS "--Werror cross-execution-space-call --expt-extended-lambda --expt-relaxed-constexpr -ccbin=mpiicpc -I${CUDA_HOME}/include -L${CUDA_HOME}/lib64 -lcurand")
-      set(CMAKE_CUDA_FLAGS_RELEASE "--Werror cross-execution-space-call --expt-extended-lambda --expt-relaxed-constexpr -ccbin=mpiicpc -I${CUDA_HOME}/include -L${CUDA_HOME}/lib64 -lcurand -arch sm_80")
-      set(CMAKE_CUDA_FLAGS_DEBUG   "--Werror cross-execution-space-call --expt-extended-lambda --expt-relaxed-constexpr -ccbin=mpiicpc -I${CUDA_HOME}/include -L${CUDA_HOME}/lib64 -lcurand -arch sm_80")
+  # NOTE: Enabling the link-time-optimization prolongs link times significantly!
+  if(GEMPIC_USE_LTO)
+    include(CheckIPOSupported)
+    check_ipo_supported(RESULT IPO_IS_SUPPORTED OUTPUT IPO_ERROR_MSG)
+    if(IPO_IS_SUPPORTED)
+      message(STATUS "IPO/LTO enabled")
+      set_target_properties(${TARGET} PROPERTY CMAKE_INTERPROCEDURAL_OPTIMIZATION ON)
+    else()
+      message(STATUS "IPO/LTO not supported: ${IPO_ERROR_MSG}")
     endif()
   endif()
+  if(GEMPIC_USE_HIP)
+    target_compile_definitions(${TARGET} PRIVATE -DGEMPIC_GPU)
+  endif()
+  if(GEMPIC_USE_CUDA)
+    target_compile_definitions(${TARGET} PRIVATE -DGEMPIC_GPU)
+    target_compile_options(${TARGET} PRIVATE -lcusparse -lcurand)
+    # The following flags are not found in the official nvcc documentation.
+    # We found the usage here
+    # https://stackoverflow.com/questions/14831051/how-to-disable-a-specific-nvcc-compiler-warnings
+    # NVCC('s version of EDG?) is very trigger happy and does not respect [[maybe_unused]].
+    # Warning tag name found at:
+    # https://web.archive.org/web/20200605121301/www.ssl.berkeley.edu/~jimm/grizzly_docs/SSL/opt/intel/cc/9.0/lib/locale/en_US/mcpcom.msg
+    target_compile_options(${TARGET} PRIVATE "SHELL:-Xcudafe --diag_suppress=set_but_not_used")
+    target_compile_options(${TARGET} PRIVATE "SHELL:-Xcudafe --diag_suppress=declared_but_not_referenced")
+  endif()
+endfunction()
 
-  find_package(CUDAToolkit REQUIRED)
-  # NVCC('s version of EDG?) is very trigger happy and does not respect [[maybe_unused]].
-  # Warning tag name found at:
-  # http://www.ssl.berkeley.edu/~jimm/grizzly_docs/SSL/opt/intel/cc/9.0/lib/locale/en_US/mcpcom.msg
-  set(CMAKE_CUDA_FLAGS "${CMAKE_CUDA_FLAGS} -lcusparse -lcurand -Xcudafe --diag_suppress=set_but_not_used -Xcudafe --diag_suppress=declared_but_not_referenced" )
-
-  # This doesn't do anything but avoid an AMReX warning -- CMAKE_CUDA_HOST_COMPILER must be specified as a -D option on the first invocation of cmake as it is used during the compiler detection process.
-  if (CMAKE_CUDA_HOST_COMPILER)
-    if ("${CMAKE_CXX_COMPILER}" MATCHES "${CMAKE_CUDA_HOST_COMPILER}")
-      set(CMAKE_CUDA_HOST_COMPILER ${CMAKE_CXX_COMPILER})
+function(gempic_set_clang_toolchain_flags_on_mpcdf_systems)
+  if(    (${CMAKE_CXX_COMPILER_ID} MATCHES "Clang.*")
+      OR (${CMAKE_CXX_COMPILER_ID} MATCHES "IntelLLVM.*"))
+    if(   ($ENV{HOSTNAME} MATCHES "raven*")
+       OR ($ENV{HOSTNAME} MATCHES "viper*")
+       OR (DEFINED ENV{MPCDF_RUNNER}))
+      set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} --gcc-toolchain=$ENV{GCC_HOME}" PARENT_SCOPE)
+      set(CMAKE_INSTALL_RPATH_USE_LINK_PATH ON PARENT_SCOPE)
     endif()
   endif()
-endif()
+endfunction()
+
+gempic_set_default_install_directories()
